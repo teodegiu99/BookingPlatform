48cb06dcbd3d401cefa415e01a61d2028099b410
teodegiu99 <dev.matteodegiuseppe@gmail.com>
2025-04-10 17:28:19 +0200

    slots

--- components/protected/slots.tsx
+++ components/protected/slots.tsx
@@ -4,11 +4,13 @@ import React from 'react'
 import { generateTimeSlotsForDay } from '../utils/timeslots'
 
 export default function TimeSlotList() {
+  //da aggiungere cointrollo che non vada nel passato, aggiungere un context per passare i dati del giorno selezionato dal componente calendario a questo 
+  //passare a generatetimeslotsforday il giorno selezionato a calendario
   const slots = generateTimeSlotsForDay('21/4/2025')
 
   return (
     <div>
-      <h2>Fasce orarie per il 21/4/2025</h2>
+      <h2>Slot disponibili</h2>
       <ul>
         {slots.map((slot, index) => (
           <li key={index}>
