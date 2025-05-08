4bd739799ff44f545df20c2ed2c70126ab3f34d5
teodegiu99 <dev.matteodegiuseppe@gmail.com>
2025-05-08 12:58:52 +0200

    vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,9 @@ export default function RootLayout({
 	
   return (
     <html lang="en">
-                  <Providers>
+                  
 
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}><Providers>{children} </Providers></body>
       </Providers>
 
     </html>
