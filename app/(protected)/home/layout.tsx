320727c9ff0f05ef650f17a3781796a379d6408a
teodegiu99 <dev.matteodegiuseppe@gmail.com>
2025-05-26 17:30:52 +0200

    no scrollbar no favicon

--- app/(protected)/home/layout.tsx
+++ app/(protected)/home/layout.tsx
@@ -3,7 +3,7 @@ import NavBar from "@/components/ui/navbar";
 
 const HomeLayout = ({children}: {children: React.ReactNode}) => {
     return ( 
-        <div className="h-screen w-screen justify-center items-center bg-[#f9f9f9] ">
+        <div className="h-screen w-screen justify-center items-center bg-[#f9f9f9] scroll-hidden">
             <NavBar />
           {children}
             <Footer />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -47,7 +47,9 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"),
+    require('tailwind-scrollbar-hide'),
+  ],
 } satisfies Config
 
 export default config
