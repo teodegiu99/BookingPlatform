4dfe29b4cda4ada445252e38d9de4cf6b186f335
teodegiu99 <dev.matteodegiuseppe@gmail.com>
2025-05-29 10:21:32 +0200

    fix help

--- components/ui/helpcom.tsx
+++ components/ui/helpcom.tsx
@@ -5,13 +5,10 @@ import { ChevronUpIcon } from '@heroicons/react/20/solid';
 import { IoIosHelpCircleOutline } from "react-icons/io";
 
 
-
 const items = [
-  { title: 'Creare un appuntamento', content: 'Scegli la riga del commerciale e clicca sul riquadro in cui vorresti piazzare l\' appuntamento. Compila i campi richiesti \(Solo la mail e l\'azienda sono obbligatori\) e clicca su invia. Attenzione, una volta creato l\' appuntamento verrà mandata un email di conferma sia al commerciale che al cliente' },
-  { title: 'Impostare il colore di un commerciale', content: 'Clicca sul nome del commerciale e seleziona il colore. Una volta assegnato il colore al commerciale, non sarà più neccessario riperterlo per ogni appuntamento.' },
-  { title: 'Come trovo un appuntamento', content: 'Clicca sulla barra di ricerca, e cerca in base al nome del commerciale, per azienda, nome del cliente o email.' },
-];
-
+  { title: 'Creare un appuntamento', content: 'Seleziona il giorno sul calendario, e scegli uno degli slot disponibili. Attenzione, una volta creato l\' appuntamento verrà mandata un email di conferma sia sulla mail aziendale che al cliente' },
+  { title: 'Trovare un appuntamento prenotato', content: 'Per vedere i dettagli si un appuntamento clicca sugli slot occupati oppure cercalo nella barra di ricerca filtrando per azienda, nome cliente, email o data.' },];
+ 
 
 const Helpcom = () => {
     const [isOpen, setIsOpen] = useState(false);
