bb21703f58d11d5a7bb4cff708406e2f888d13f4
teodegiu99 <dev.matteodegiuseppe@gmail.com>
2025-06-23 18:17:08 +0200

    fix grafico

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,6 +1,6 @@
 const AuthLayout = ({children}: {children: React.ReactNode}) => {
     return ( 
-        <div className="h-full flex items-center justify-center bg-[url('/BG.png')] bg-cover bg-center">
+        <div className="h-screen flex items-center justify-center bg-[url('/BG.png')] bg-cover bg-center">
             {children}
         </div>
      );
--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -11,7 +11,7 @@ const Calendar = async () => {
   const session = await auth();
   return (
 <DateProvider>
-        <div className="w-full h-full flex justify-center items-center">
+        <div className="w-full h-full flex justify-center items-center pt-16">
         <div className="grid grid-cols-3 xl:grid-cols-6 xl:gap-x-8 max-w-[1280px] p-8 gap-x-4">
           {/* Colonna sinistra */}
           <div className="col-span-1 xl:col-span-2 ">
