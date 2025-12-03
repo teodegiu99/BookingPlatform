04339ca11d277294bff643a4ec57600a6a711eb6
teodegiu99 <dev.matteodegiuseppe@gmail.com>
2025-12-03 12:09:59 +0100

    pitti inverno

--- app/(protected)/home/page.tsx
+++ app/(protected)/home/page.tsx
@@ -1,24 +1,23 @@
-
-import { CalendarDemo } from "@/components/protected/calendar"
-import NextAppointment from "@/components/protected/NextAppointment"
-import TimeSlotList from "@/components/protected/slots"
-import { DateProvider } from '@/context/DateContext'
-import AppointmentSearch from "@/components/protected/AppointmentSearch"
-import { auth, signOut } from "@/auth";
+import { auth } from "@/auth";
 import DashboardPage from "@/app/dashboard/page"
 import Calendar from "@/app/calendar/page"
-import NavBar from "@/components/ui/navbar"
-const Home = async () => {
+
+// Definiamo il tipo per i searchParams che ci aspettiamo
+type Props = {
+  searchParams: { view?: string };
+};
+
+const Home = async ({ searchParams }: Props) => {
   const session = await auth();
 
   if (session?.user.role === 'ADMIN') {
     return (
-      
-    <DashboardPage />
+      <DashboardPage />
     )
   } else {
     return (
-      <Calendar />
+      // Passiamo i searchParams ricevuti dalla Home al Calendar
+      <Calendar searchParams={searchParams} />
     );
   }
 };
